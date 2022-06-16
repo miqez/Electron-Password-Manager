672ffa928b23fdc7b455858ea1636764c9c07ac4
Arendarski Michał <arendarskimichal1@gmail.com>
2022-06-16 18:18:16 +0200

    Add home view

--- app/router/index.js
+++ app/router/index.js
@@ -1,7 +1,18 @@
 import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
 import { IS_ELECTRON } from '@root/config';
+import ElectronHome from '@/views/ElectronHome.vue';
 
-const routes = [];
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: ElectronHome,
+  },
+  {
+    path: '/:nonExistent(.*)*',
+    component: ElectronHome,
+  },
+];
 
 const router = createRouter({
   history: IS_ELECTRON
---
+++ app/views/ElectronHome.vue
@@ -0,0 +1,88 @@
+<template>
+  <section class="home section">
+    <div class="home__wrapper wrapper">
+      <header class="home__header">
+        <img
+          class="home__logo"
+          src="@/assets/vue.png"
+          alt=""
+        >
+        <h2 class="home__heading heading heading--h2">
+          Electron Password Manager
+        </h2>
+        <p class="home__paragraph paragraph">
+          Electron application created for learning purposes
+        </p>
+      </header>
+      <button
+        class="home__button home__button--upload"
+        @click="upload"
+      >
+        Upload a file
+      </button>
+      <p class="home__paragraph paragraph">
+        Or
+      </p>
+      <router-link
+        class="home__button home__button--create"
+        :to="{ name: 'Home' }"
+      >
+        Create new
+      </router-link>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ElectronHome',
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  &__logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  &__button {
+    border: none;
+    color: $white;
+    cursor: pointer;
+    display: block;
+    font-size: 1.25rem;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 150px;
+    outline: none;
+    padding: .25em .75em;
+    text-align: center;
+    text-decoration: none;
+
+    &--upload {
+      background-color: $ocean-green;
+
+      &:focus,
+      &:hover {
+        background-color: $sea-green;
+      }
+    }
+
+    &--create {
+      background-color: $pickled-bluewood;
+
+      &:focus,
+      &:hover {
+        background-color: $pickled-bluewood-2;
+      }
+    }
+  }
+}
+</style>
